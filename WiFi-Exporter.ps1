86bc2c7f0797633f911f83c63e0b609da4e2520e
arrowxcom <watusa@arrowx.com>
2022-04-27 10:39:40 -0600

    Create WiFi-Exporter.ps1
    Original commit - v1.0

---
+++ WiFi-Exporter.ps1
@@ -0,0 +1,34 @@
+﻿
+<#
+ __  __                      _____         _     
+ \ \/ /___ _ ____  _____  __|_   _|__  ___| |__  
+  \  // _ \ '__\ \/ / _ \/ __|| |/ _ \/ __| '_ \ 
+  /  \  __/ |   >  <  __/\__ \| |  __/ (__| | | |
+ /_/\_\___|_|  /_/\_\___||___/|_|\___|\___|_| |_|
+                                                 
+WiFi Exporter - Used to export all stored SSIDs and Passwords on a Windows Machine
+Version: v1.0
+
+#>
+
+Write-Host -ForegroundColor Cyan "Querying WiFi Profiles."
+$wifi = netsh wlan show profiles
+
+[int]$count = ($wifi.Count)-10
+Write-Host -ForegroundColor Cyan $count "Profiles Found."
+Write-Host ""
+
+foreach ($record in $wifi) {
+    if ($record -match "All User Profile ") {
+        $ssid = $record -split ": "
+        $query = netsh wlan show profile $ssid[1] key=clear
+    
+        foreach ($line in $query) {
+            if ($line -match "Authentication         : Open") { Write-Host -ForegroundColor Yellow $ssid[1] " - No Password, Open Network" }
+            if ($line -match "Key Content") {
+                $key = $line -split ": "
+                Write-Host -ForegroundColor Green $ssid[1] " - " $key[1]
+            }
+        }
+    }
+}
